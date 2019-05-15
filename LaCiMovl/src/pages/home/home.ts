1975bc83b00c739c5b1b79f05693234bb8dabc20
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-05-14 23:17:04 -0400

    comienzo
    Inicio de la aplicacion

---
+++ LaCiMovl/src/pages/home/home.ts
@@ -0,0 +1,19 @@
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+import { RegistroPage} from '../registro/registro';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+
+  }
+
+  registrar(){
+  	this.navCtrl.push(RegistroPage);
+  }
+
+}
