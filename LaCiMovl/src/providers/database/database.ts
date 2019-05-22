d448c207359870f9c0e8a36a97c93bfe148e920e
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-05-21 20:55:06 -0400

    avance bd

---
+++ LaCiMovl/src/providers/database/database.ts
@@ -0,0 +1,74 @@
+import { Injectable } from '@angular/core';
+import { Http } from '@angular/http';
+import 'rxjs/add/operator/map';
+
+import { SQLite, SQLiteObject } from '@ionic-native/sqlite';
+
+/*
+  Generated class for the DatabaseProvider provider.
+  See https://angular.io/guide/dependency-injection for more info on providers
+  and Angular DI.
+*/
+@Injectable()
+export class DatabaseProvider {
+
+  private db: SQLiteObject;
+  private isOpen: boolean;
+
+  constructor(
+    public http: Http,
+    public storage: SQLite
+  ) {
+    if (!this.isOpen) {
+      this.storage = new SQLite();
+      this.storage.create({ name: "data.db", location: "default" }).then((db: SQLiteObject) => {
+        this.db = db;
+        db.executeSql("CREATE TABLE IF NOT EXISTS users (correo TEXT, pass TEXT)", []);
+        this.isOpen = true;
+      }).catch((error) => {
+        console.log(error);
+      })
+    }
+  }
+
+  CreateUser(correo:string, pass:string){
+    return new Promise ((resolve, reject) => {
+      let sql = "INSERT INTO users (correo, pass) VALUES (?, ?)";
+      this.db.executeSql(sql, [correo, pass]).then((data) =>{
+        resolve(data);
+      }, (error) => {
+        reject(error);
+      });
+    });
+  }
+
+  GetAllUsers(){
+    return new Promise ((resolve, reject) => {
+      this.db.executeSql("SELECT * FROM users", []).then((data) => {
+        let arrayUsers = [];
+        if (data.rows.length > 0) {
+          for (var i = 0; i < data.rows.length; i++) {
+            arrayUsers.push({
+              correo: data.rows.item(i).correo,
+              pass: data.rows.item(i).pass
+            });            
+          }          
+        }
+        resolve(arrayUsers);
+      }, (error) => {
+        reject(error);
+      })
+    })
+  }
+
+  DeleteUser(correo:string){
+    return new Promise ((resolve, reject) => {
+      this.db.executeSql("DELETE * FROM users WHERE correo=(?)", [correo]).then((data) => {
+        resolve(data);
+      },(error) => {
+        reject(error);
+      })
+    })
+  }
+
+}
