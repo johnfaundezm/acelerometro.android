b295f7e1934434aff03ee370451201c3bce02eb6
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-05-31 09:12:11 -0400

    migracion de vista
    se migran las vistas de deportistas y de entrenadores de los archivos .ts

--- LaCiMovl/src/pages/deportistas/deportistas.ts
+++ LaCiMovl/src/pages/deportistas/deportistas.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
-
+import { WebservicesProvider } from '../../providers/webservices/webservices';
 /**
  * Generated class for the DeportistasPage page.
  *
@@ -15,11 +15,47 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DeportistasPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  deportista: Array<{email:string,pass:string,nombre:string,apellido_p:string,apellido_m:string,genero:string,edad:string,peso:string,estatura:string,imc:string,pais:string,estado:string,fecha_r:string}>=[{email:'',pass:'',nombre:'',apellido_p:'',apellido_m:'',genero:'',edad:'',peso:'',estatura:'',imc:'',pais:'',estado:'',fecha_r:''}];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private webservices: WebservicesProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad DeportistasPage');
+    while(this.deportista.length>0){
+      this.deportista.pop();
+    }
+    this.vista_deportista();
+  }
+
+  vista_deportista(){
+    this.webservices.vista_deportista().then(
+      (datos) =>{
+        let largo=Object.keys(datos).length;
+        for(var i=0;i<largo;i++){
+          var email= datos[i].CORREO;
+          var pass= datos[i].PASS;
+          var nombre= datos[i].NOMBRE;
+          var apellido_p= datos[i].APELLIDO_P;
+          var apellido_m= datos[i].APELLIDO_M;
+          var genero= datos[i].GENERO;
+          var edad= datos[i].EDAD;
+          var peso= datos[i].PESO;
+          var estatura= datos[i].ESTATURA;
+          var imc= datos[i].IMC;
+          var pais= datos[i].PAIS;
+          var estado= datos[i].ESTADO;
+          var fecha_r= datos[i].FECHA_R;
+
+          this.deportista.push({"email":email,"pass":pass,"nombre":nombre,"apellido_p":apellido_p,"apellido_m":apellido_m,"genero":genero,"edad":edad,"peso":peso,"estatura":estatura,"imc":imc,"pais":pais,"estado":estado,"fecha_r":fecha_r});
+        }
+      },
+      (err)=>{
+        alert(JSON.stringify(err))
+      })
+  }
+
+  detalle(email){
+    alert(email);
   }
 
 }
--- LaCiMovl/src/pages/entrenadores/entrenadores.ts
+++ LaCiMovl/src/pages/entrenadores/entrenadores.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { WebservicesProvider } from '../../providers/webservices/webservices';
 
 /**
  * Generated class for the EntrenadoresPage page.
@@ -15,11 +16,45 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class EntrenadoresPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  entrenador: Array<{email:string,pass:string,nombre:string,apellido_p:string,apellido_m:string,genero:string,edad:string,pais:string,estado:string,fecha_r:string}>=[{email:'',pass:'',nombre:'',apellido_p:'',apellido_m:'',genero:'',edad:'',pais:'',estado:'',fecha_r:''}];
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private webservices: WebservicesProvider) {
   }
 
   ionViewDidLoad() {
-    console.log('ionViewDidLoad EntrenadoresPage');
+    while(this.entrenador.length>0){
+      this.entrenador.pop();
+    }
+
+    this.vista_entrenador();
+  }
+
+  vista_entrenador(){
+    this.webservices.vista_entrenador().then(
+      (datos) =>{
+        let largo=Object.keys(datos).length;
+        for(var i=0;i<largo;i++){
+          var email= datos[i].CORREO;
+          var pass= datos[i].PASS;
+          var nombre= datos[i].NOMBRE;
+          var apellido_p= datos[i].APELLIDO_P;
+          var apellido_m= datos[i].APELLIDO_M;
+          var genero= datos[i].GENERO;
+          var edad= datos[i].EDAD;
+          var pais= datos[i].PAIS;
+          var estado= datos[i].ESTADO;
+          var fecha_r= datos[i].FECHA_R;
+
+          this.entrenador.push({"email":email,"pass":pass,"nombre":nombre,"apellido_p":apellido_p,"apellido_m":apellido_m,"genero":genero,"edad":edad,"pais":pais,"estado":estado,"fecha_r":fecha_r});           
+        }
+      },
+      (err)=>{
+        alert(JSON.stringify(err))
+      })
+  }
+
+  detalle(email){
+    alert(email);
   }
 
 }
