cbe2d6e6f62960f3af358c70bf7aba4a9b1c2972
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-06-24 00:37:09 -0400

    Update deportistasent.ts

--- LaCiMovl/src/pages/deportistasent/deportistasent.ts
+++ LaCiMovl/src/pages/deportistasent/deportistasent.ts
@@ -12,8 +12,7 @@ export class DeportistasentPage {
   enlaces: Array<{email:string, entrenamiento_n:string, fecha:string}>=[{email:'', entrenamiento_n:'', fecha:''}];
   correo:any;
 
-  searchQuery: string = '';
-  items: Array<{email_dep:string}>=[{email_dep:''}];
+  items: string[];
 
   actividades: string ='deportistas';
 
@@ -42,7 +41,7 @@ export class DeportistasentPage {
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.items = this.items.filter((item) => {
-        return (item.email_dep.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
   }
@@ -70,9 +69,11 @@ export class DeportistasentPage {
       (datos)=>{
         //alert(JSON.stringify(datos));
         let largo=Object.keys(datos).length;
+        var j:number=0;
         for(var i=0;i<largo;i++){
-          var email_dep= datos[i].CORREO;
-          this.items.push({"email_dep":email_dep});
+          var email_dep:string= datos[i].CORREO;          
+          this.items[j]=email_dep;
+          j++;
         }
       },
       (err)=>{
