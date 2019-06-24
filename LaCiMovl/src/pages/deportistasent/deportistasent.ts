fc09e26a473eaf30afe9f24e5c50bed8b7eb6dc3
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-06-23 23:57:02 -0400

    Update deportistasent.ts

--- LaCiMovl/src/pages/deportistasent/deportistasent.ts
+++ LaCiMovl/src/pages/deportistasent/deportistasent.ts
@@ -34,7 +34,7 @@ export class DeportistasentPage {
 
   getItems(ev: any) {
     // Reset items back to all of the items
-    //this.initializeItems();
+    this.consulta_deportistas();
 
     // set val to the value of the searchbar
     const val = ev.target.value;
@@ -42,7 +42,7 @@ export class DeportistasentPage {
     // if the value is an empty string don't filter the items
     if (val && val.trim() != '') {
       this.items = this.items.filter((item) => {
-        return (item.toLowerCase().indexOf(val.toLowerCase()) > -1);
+        return (item.email_dep.toLowerCase().indexOf(val.toLowerCase()) > -1);
       })
     }
   }
