73d720132095cccb4380a450e652217b62fcb3e9
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-06-25 01:43:17 -0400

    Update webservices.ts

--- LaCiMovl/src/providers/webservices/webservices.ts
+++ LaCiMovl/src/providers/webservices/webservices.ts
@@ -518,7 +518,7 @@ export class WebservicesProvider {
       //en el body se colocan las variables que van a hacer enviadas al php que se encuentra en el servidor, en el formato nombre de variable recibida igual al nombre de la variable que se envia
       let body = 'correo_solicitado='+correo_solicitado +'&correo_solicitante='+correo_solicitante +'&fecha_solicitud='+fecha_solicitud;
       //en la url se ingresa la direccion exacta del servidor en donde se encuentra el archivo php que se va a utilizar para recibir las variables
-      let url = "http://192.81.216.141/webservices/insert_usuario.php";
+      let url = "http://192.81.216.141/webservices/insert_solicitud.php";
 
       this.http.post(url, body, options)// se envia la url el body y el options por medio de un post al servidor
         .map(res => res.json()) // se retorno el body como text y no como json por error en el formato de json en la pagina
