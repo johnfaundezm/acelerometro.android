8def64fb41ce479c669be418dbf6fc6a18dd883f
alfonsobilocopetiuc <45896045+alfonsobilocopetiuc@users.noreply.github.com>
2019-07-01 22:26:05 -0400

    ajustes varios

--- LaCiMovl/src/pages/infoentrenamiento/infoentrenamiento.ts
+++ LaCiMovl/src/pages/infoentrenamiento/infoentrenamiento.ts
@@ -18,6 +18,8 @@ import { THIS_EXPR } from '@angular/compiler/src/output/output_ast';
 })
 export class InfoentrenamientoPage {
 
+  tabBarElement: any; //variable que almacena el elemento tabbar
+
   public tipo_entrenamiento: String;
   public tiempo_entrenamiento: number;
   public unidad_entrenamiento: number;
@@ -39,10 +41,19 @@ export class InfoentrenamientoPage {
   constructor(public navCtrl: NavController, public navParams: NavParams, private webservices: WebservicesProvider,
     public loadingCtrl: LoadingController) {
     this.ide = this.navParams.get('ide');
+    this.tabBarElement = document.querySelector('.tabbar.show-tabbar'); //se pasa el elemento tabbar a la variable antes declarada
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad InfoentrenamientoPage');
+  }
+  //antes de entrar a la vista se oculta el tabbar
+  ionViewWillEnter() {
+    this.tabBarElement.style.display = 'none';
+  }
+  //cuando va a salir de la vista se le agrega el tabbar nuevamente
+  ionViewWillLeave() {
+    this.tabBarElement.style.display = 'flex';
   }
 
   loadregistrar() {
@@ -129,6 +140,10 @@ export class InfoentrenamientoPage {
   detalletiempo(){
     this.tiempo_recuperacion= (this.tiempoRM*60) + this.tiempoRS;
     this.tiempo_entrenamiento=(this.tiempoM*60) + this.tiempoS;
+  }
+
+  volver(){
+    this.navCtrl.pop();
   }
 
 
