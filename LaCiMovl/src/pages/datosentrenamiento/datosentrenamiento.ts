a2ad8b3ff317001a1a4732da8d274f79e0907f34
alfonsobilocopetiuc <45896045+alfonsobilocopetiuc@users.noreply.github.com>
2019-07-02 20:09:06 -0400

    update vista datosentrenamiento

--- LaCiMovl/src/pages/datosentrenamiento/datosentrenamiento.ts
+++ LaCiMovl/src/pages/datosentrenamiento/datosentrenamiento.ts
@@ -8,11 +8,28 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class DatosentrenamientoPage {
 
+  tabBarElement: any; //variable que almacena el elemento tabbar
+
   constructor(public navCtrl: NavController, public navParams: NavParams) {
+    this.tabBarElement = document.querySelector('.tabbar.show-tabbar'); //se pasa el elemento tabbar a la variable antes declarada
+
   }
 
   ionViewDidLoad() {
     console.log('ionViewDidLoad DatosentrenamientoPage');
+  }
+
+  //antes de entrar a la vista se oculta el tabbar
+  ionViewWillEnter() {
+    this.tabBarElement.style.display = 'none';
+  }
+  //cuando va a salir de la vista se le agrega el tabbar nuevamente
+  ionViewWillLeave() {
+    this.tabBarElement.style.display = 'flex';
+  }
+
+  volver(){
+    this.navCtrl.pop();
   }
 
 }
