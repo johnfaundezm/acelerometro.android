f2db018a4130bc022ac0a4ca4a2f7ab0e0079f47
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-07-04 21:21:16 -0400

    Update infoentrenamiento.ts

--- LaCiMovl/src/pages/infoentrenamiento/infoentrenamiento.ts
+++ LaCiMovl/src/pages/infoentrenamiento/infoentrenamiento.ts
@@ -89,6 +89,7 @@ export class InfoentrenamientoPage {
     this.loading = this.loadingCtrl.create({
       spinner: 'ios',
       content: 'Entrando al entrenamiento...',
+      dismissOnPageChange: true
     });
 
     this.loading.onDidDismiss(() => {
