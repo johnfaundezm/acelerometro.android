5195990eb86fa57e41e658f06a66096939dc9c62
alfonsobilocopetiuc <45896045+alfonsobilocopetiuc@users.noreply.github.com>
2019-07-05 02:55:27 -0400

    Update fourier.ts

--- LaCiMovl/src/pages/fourier/fourier.ts
+++ LaCiMovl/src/pages/fourier/fourier.ts
@@ -2,13 +2,6 @@ import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams } from 'ionic-angular';
 
 
-/**
- * Generated class for the FourierPage page.
- *
- * See https://ionicframework.com/docs/components/#navigation for more info on
- * Ionic pages and navigation.
- */
-
 @IonicPage()
 @Component({
   selector: 'page-fourier',
@@ -34,6 +27,7 @@ export class FourierPage {
   giroscopioFFT(){
 
   }
+  /*
   fft2(acelerometroDatos) {
     var N = acelerometroDatos.length;
     if (N <= 1) {
@@ -97,5 +91,5 @@ export class FourierPage {
   }
   console.log(Yr);
   }
-  
+  */
 }
