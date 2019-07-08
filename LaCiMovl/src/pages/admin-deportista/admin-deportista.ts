0134227578a71dfe6c1b2cde7f5837df90641f7a
John Michael Faundez Miño <45896099+lnboxDl@users.noreply.github.com>
2019-07-08 14:20:53 -0400

    comentando 7

--- LaCiMovl/src/pages/admin-deportista/admin-deportista.ts
+++ LaCiMovl/src/pages/admin-deportista/admin-deportista.ts
@@ -1,3 +1,4 @@
+// se importan los plugins que se ejecutarán en esta vista
 import { Component } from '@angular/core';
 import { IonicPage, NavController, NavParams, PopoverController } from 'ionic-angular';
 import { WebservicesProvider } from '../../providers/webservices/webservices';
@@ -11,6 +12,7 @@ import { PoadmindepComponent } from '../../components/poadmindep/poadmindep';
 })
 export class AdminDeportistaPage {
 
+  //DECLARACION DE VARIABLES
   tabBarElement: any; //variable que almacena el elemento tabbar
 
   correo:any;
@@ -28,17 +30,20 @@ export class AdminDeportistaPage {
   estado:any;
   fecha_r:any;
 
+  //Constructor, donde se declaran todos los plugins
   constructor(public navCtrl: NavController, public navParams: NavParams, private webservices: WebservicesProvider, public popoverCtrl: PopoverController) {
+    //recibe parámeros de la vista anterior
     this.correo = this.navParams.get('correo');
     this.tabBarElement = document.querySelector('.tabbar.show-tabbar'); //se pasa el elemento tabbar a la variable antes declarada
   }
   
   //antes de entrar a la vista se oculta el tabbar
-  ionViewWillEnter() {
+  ionViewWillEnter() {// evento que se ejecuta antes de entrar a la vista
+    this.detalle(); // método que trae los datos del deportista
     this.tabBarElement.style.display = 'none';
   }
   //cuando va a salir de la vista se le agrega el tabbar nuevamente
-  ionViewWillLeave() {
+  ionViewWillLeave() {// evento que se ejecuta al salir de la vista actual
     this.tabBarElement.style.display = 'flex';
   }
 
@@ -49,35 +54,31 @@ export class AdminDeportistaPage {
     })
   }
 
-  ionViewCanEnter() {
-    this.detalle();
-  }
-
-  doRefresh(refresher) {
+  doRefresh(refresher) {// método que actualiza al tirar hacia abajo
     this.detalle();
     refresher.complete();
   }
   //información de perfil de deportista
   detalle(){
-    this.webservices.consulta_deportista(this.correo).then(
-      (datos)=>{
+    this.webservices.consulta_deportista(this.correo).then( // se envian todos los parametros que se ven en el paréntesis
+      (datos)=>{// se reciben los datos de respuesta del servidor
         //alert(JSON.stringify(datos));
-        this.email= datos[0].CORREO;
-        this.pass= datos[0].PASS;
-        this.nombre= datos[0].NOMBRE;
-        this.apellido_p= datos[0].APELLIDO_P;
-        this.apellido_m= datos[0].APELLIDO_M;
-        this.genero= datos[0].GENERO;
-        this.edad= datos[0].EDAD;
-        this.peso= datos[0].PESO;
-        this.estatura= datos[0].ESTATURA;
-        this.imc= datos[0].IMC;
-        this.pais= datos[0].PAIS;
-        this.estado= datos[0].ESTADO;
-        this.fecha_r= datos[0].FECHA_R;
+        this.email= datos[0].CORREO; // se almacena el dato en una variable
+        this.pass= datos[0].PASS; // se almacena el dato en una variable
+        this.nombre= datos[0].NOMBRE; // se almacena el dato en una variable
+        this.apellido_p= datos[0].APELLIDO_P; // se almacena el dato en una variable
+        this.apellido_m= datos[0].APELLIDO_M; // se almacena el dato en una variable
+        this.genero= datos[0].GENERO; // se almacena el dato en una variable
+        this.edad= datos[0].EDAD; // se almacena el dato en una variable
+        this.peso= datos[0].PESO; // se almacena el dato en una variable
+        this.estatura= datos[0].ESTATURA; // se almacena el dato en una variable
+        this.imc= datos[0].IMC; // se almacena el dato en una variable
+        this.pais= datos[0].PAIS; // se almacena el dato en una variable
+        this.estado= datos[0].ESTADO; // se almacena el dato en una variable
+        this.fecha_r= datos[0].FECHA_R; // se almacena el dato en una variable
       },
       (err)=>{
-        alert(JSON.stringify(err))
+        alert(JSON.stringify(err)) // si ocurre un error de comunicacion con el servidor, se envia este mensaje
       })
   }
   //Calcula el IMC según la masa corporal del deportista.
@@ -106,6 +107,7 @@ export class AdminDeportistaPage {
     }
   }
 
+  //método que lleva a la vista anterior
   volver() {
     this.navCtrl.pop();
   }
